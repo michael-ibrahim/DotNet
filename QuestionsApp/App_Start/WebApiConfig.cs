0f3bac6bbd384825e6662f8e05aaa5f7797dd39e
Michael-Ibrahim-1992 <michael.ibrahim@imperialcreation.com>
2018-02-21 20:01:35 +0200

    adding files

---
+++ QuestionsApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Net.Http.Headers;
+using System.Web.Http.Cors;
+
+namespace QuestionsApp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //enabling CORS
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //Defaulting the return format to JSON
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                 .Add(new MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
---
+++ QuestionsApp/Controllers/MainController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DbAccess3;
+
+
+namespace QuestionsApp.Controllers
+{
+    //Get /lessons
+    public class LessonsController : ApiController
+    {
+        public IEnumerable<lesson> GET()
+        {
+           using(Task3Entities entities = new Task3Entities())
+          {
+              return entities.lessons.ToList();
+          }
+        }
+    }
+
+    //Get /goals
+    public class GoalsController : ApiController
+    {
+        public IEnumerable<goal> GET()
+        {
+            using (Task3Entities entities = new Task3Entities())
+            {
+                return entities.goals.ToList();
+            }
+        }
+    }
+
+    //Get /questions
+    public class QuestionsController : ApiController
+    {
+        public IEnumerable<question> GET()
+        {
+            using (Task3Entities entities = new Task3Entities())
+            {
+                return entities.questions.ToList();
+            }
+        }
+    }
+
+    //Post postQuestions
+    public class PostQuestionController : ApiController
+    {
+        //Gets the question parameter from the body
+        public question POST([FromBody] question question)
+        {
+            using (Task3Entities entities = new Task3Entities())
+            {
+                //get the highest Id in the table + 1 => new insert ID (brute force method)
+                var maxId = entities.questions.OrderByDescending(u => u.Id).FirstOrDefault();
+                question.Id = maxId.Id + 1;
+                entities.questions.Add(question);
+                entities.SaveChanges();
+                return question;
+            }
+        }
+    }
+
+}
+
